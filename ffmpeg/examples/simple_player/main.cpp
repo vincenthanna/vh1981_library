e45c3cd88447dbfe0d27ecf56a1a58ab1f7a8cb1
김연희 <vh1981@idis.co.kr>
2018-02-26 18:25:46 +0900

    ffmepg 예제 코드 추가

---
+++ ffmpeg/examples/simple_player/main.cpp
@@ -0,0 +1,21 @@
+extern "C"{
+    #include <libavformat/avformat.h> 
+    #include <libavcodec/avcodec.h>
+    #include <libswresample/swresample.h>
+    #include <libswscale/swscale.h>
+    #include <libavutil/opt.h>
+};
+
+#pragma comment(lib,"avformat.lib")
+#pragma comment(lib,"avcodec.lib")
+#pragma comment(lib,"swresample.lib")
+#pragma comment(lib,"swscale.lib")
+#pragma comment(lib,"avutil.lib")
+
+
+int main(int argc, char** argv)
+{    
+    av_register_all();
+    avcodec_register_all();
+    return 0;
+}
