f0803c57861f83c044f516dc74011be1953e365b
김연희 <gim-yeonhui@gim-yeonhuiui-MacBook.local>
2018-02-28 03:49:53 +0900

    ffmpeg examples

--- ffmpeg/examples/simple_player/main.cpp
+++ ffmpeg/examples/simple_player/main.cpp
@@ -23,10 +23,24 @@ struct buffer_data {
     size_t size; ///< size left in the buffer
 };
 
+static int read_packet(void *opaque, uint8_t *buf, int buf_size)
+{
+    struct buffer_data *bd = (struct buffer_data *)opaque;
+    buf_size = FFMIN(buf_size, bd->size);
+    if (!buf_size)
+        return AVERROR_EOF;
+    printf("ptr:%p size:%zu\n", bd->ptr, bd->size);
+    /* copy internal buffer data to buf */
+    memcpy(buf, bd->ptr, buf_size);
+    bd->ptr  += buf_size;
+    bd->size -= buf_size;
+    return buf_size;
+}
+
 int main(int argc, char** argv)
 {   
-    AVFormatContext *fmtCtx = NULL;
-    AVIOContext *avioCtx = NULL;
+    AVFormatContext* fmtCtx = NULL;
+    AVIOContext* avioCtx = NULL;
     uint8_t *buf = NULL, *avioCtxBuf = NULL;
     size_t bufSize = 0, avioCtxBufSize = 4096;
     char* inputFileName = NULL;
@@ -38,9 +52,60 @@ int main(int argc, char** argv)
     // slurp file content into buffer:
     ret = av_file_map(inputFileName, &buf, &bufSize, 0, NULL);
     if (ret < 0) {
-        printf("[Error] ret=%d\n", ret);
+        printf("[Error] line : %d ret=%d\n", __LINE__, ret);
         return -1;
     }
+
+    // fill opaque structure used by the AVIOContext read callback
+    bd.ptr = buf;
+    bd.size = bufSize;
+
+    if (!(fmtCtx = avformat_alloc_context())) {
+        printf("[Error] line : %d ret=%d\n", __LINE__, ret);
+        return -1;
+    }
+
+    if (!(avioCtxBuf = (uint8_t*)av_malloc(avioCtxBufSize))) {
+        printf("[Error] line : %d ret=%d\n", __LINE__, ret);
+        return -1;
+    }
+
+    avioCtx = avio_alloc_context(avioCtxBuf, 
+        avioCtxBufSize, 
+        0,
+        &bd,
+        &read_packet,
+        NULL,
+        NULL);
+
+    if (!avioCtx) {
+        printf("[Error] line : %d ret=%d\n", __LINE__, ret);
+        return -1;
+    }
+
+    fmtCtx->pb = avioCtx;
+
+    ret = avformat_open_input(&fmtCtx, NULL, NULL, NULL);
+    if (ret < 0) {
+        printf("[Error] line : %d ret=%d\n", __LINE__, ret);
+        return -1;
+    }
+
+    ret = avformat_find_stream_info(fmtCtx, NULL);
+    if (ret < 0) {
+        printf("[Error] line : %d ret=%d\n", __LINE__, ret);
+        return -1;
+    }
+
+    av_dump_format(fmtCtx, 0, inputFileName, 0);
+
+    avformat_close_input(&fmtCtx);
+    if (avioCtx) {
+        av_freep(&avioCtx->buffer);
+        av_freep(&avioCtx);
+    }
+    av_file_unmap(buf, bufSize);
+
     printf("end of main()\n");
     return 0;
 }
