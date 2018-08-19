c01e8f145aea8d48b9d34a388ffa133219ed9a90
vincenthanna <firsttimelove@gmail.com>
2018-08-20 00:26:07 +0900

    cnn 동작되는 것은 확인 - 졍확도 0.28

--- python/opencv/process01.py
+++ python/opencv/process01.py
@@ -27,42 +27,59 @@ from PIL import Image
 categoryCnt = len(imageDirs)
 
 
-X_train = np.empty((0, 96, 96, 3), float)
-t_train = np.empty((0, 1, 5))
+# X_train = np.empty((1, 96, 96, 3), float)
+# t_train = np.empty((0, 1, 5))
+#
+# X_test = np.empty([])
+# t_test = np.empty([])
+X_train = np.empty((0,96,96,3))
+t_train = np.empty((0, categoryCnt))
 
-X_test = np.empty([])
-t_test = np.empty([])
+X_test = np.empty((0,96,96,3))
+t_test = np.empty((0, categoryCnt))
 
 X = tf.placeholder(tf.float32, [None, 96, 96, 3])
 Y = tf.placeholder(tf.float32, [None, categoryCnt])
 keep_prob = tf.placeholder(tf.float32)
 
-def resize_img(imageFilePath):
-    image = cv2.imread(imageFilePath)
-    cv2.resize(image, (96, 96))
-    cv2.imshow(imageFilePath, image)
-    cv2.waitKey(0)
+# def resize_img(imageFilePath):
+#     image = cv2.imread(imageFilePath)
+#     cv2.resize(image, (96, 96))
+#     cv2.imshow(imageFilePath, image)
+#     cv2.waitKey(0)
+
+def label_to_onehot(label, depth):
+    onehot = np.zeros((depth), float)
+    onehot[label] = 1
+    return onehot
+
+print(label_to_onehot(0, 5))
+print(label_to_onehot(1, 5))
+print(label_to_onehot(2, 5))
+
 
 def get_img(imageFilePath):
-    #imageFilePath = os.path.join(imageDirPath, f)
-    #image = tf.image.decode_jpeg(tf.read_file(imageFilePath), channels=3)
-    #image = cv2.imread(imageFilePath)
-    #image = mpimg.imread(imageFilePath)
     image = Image.open(imageFilePath)
     image = image.resize((96,96))
     image = np.asarray(image)
 
-    print("image.shape=", image.shape)
+    #print("image.shape=", image.shape)
 
     return image
 
+
 def load_data():
     dataIndex = 0
+    xtrain = []
+    ttrain = []
+    xtest = []
+    ttest = []
     for index, imgdir in enumerate(imageDirs):
 
         print(index, imgdir)
         imageDirPath = os.path.join(imgdir, "cropped")
-        files = [f for f in os.listdir(imageDirPath) if os.path.isfile(os.path.join(imageDirPath, f))]
+        files = [f for f in os.listdir(imageDirPath)
+                 if os.path.isfile(os.path.join(imageDirPath, f)) and f.find("jpeg") > 0]
         label = index
 
         totalCnt = len(files)
@@ -71,50 +88,87 @@ def load_data():
         trainCnt = totalCnt * 3 / 4
 
         cnt = 0
+
+
         for f in files:
             imageFilePath = os.path.join(imageDirPath, f)
             image = get_img(imageFilePath)
             #print("image=", image.shape)
 
             #label을 one-hot encoding으로 변경해야 함
-            onehot = tf.one_hot(indices=[label], depth=categoryCnt)
-            #print("onehot=", onehot.shape)
+            #onehot = tf.one_hot(indices=[label], depth=categoryCnt)
+            onehot = label_to_onehot(label, depth=categoryCnt)
+            image = np.reshape(image, [96, 96, 3])
 
-            print("image.shape", image.shape)
-            print("onehot.shape", onehot.shape)
-
-            image = np.reshape(image, [1, 96, 96, 3])
-            print("reshaped image : ", image.shape)
 
             if cnt < trainCnt:
-                X_train = np.append(X_train, np.reshape(image, [1, 96, 96, 3]))
-                t_train = np.append(t_train, onehot)
+                xtrain.append(image)
+                ttrain.append(onehot)
+
             else:
-                # X_test.append(image)
-                # t_test.append(onehot)
-                X_test = np.append(X_test, image)
-                t_test = np.append(t_test, onehot)
+                xtest.append(image)
+                ttest.append(onehot)
 
-            print("X_train, X_test, t_train ,t_test ", X_train.shape, X_test.shape,
-                  t_train.shape, t_test.shape)
+            cnt += 1
 
+            #print("xtrain len=", len(xtrain))
 
+            # print("X_train, X_test, t_train ,t_test ", X_train.shape, X_test.shape,
+            #       t_train.shape, t_test.shape)
+
+    print("total xtrain = ", len(xtrain))
+    global X_train
+    global t_train
+    global X_test
+    global t_test
+
+    X_train = np.asarray(xtrain)
+    t_train = np.asarray(ttrain)
+    X_test = np.asarray(xtest)
+    t_test = np.asarray(ttest)
+    print("load_data() :")
+    print("        X_train : ", X_train.shape)
+    print("        t_train : ", t_train.shape)
+    print("        X_test  : ", X_test.shape)
+    print("        t_test  : ", t_test.shape)
 
 
 def build_model():
 
-    W1 = tf.Variable(tf.random_normal([3, 3, 3, 32], stddev=0.01))
-    L1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1], padding='SAME')
+    '''
+    1st layer
+    input : 96 X 96 X 3
+    '''
+    W1 = tf.Variable(tf.random_normal([5, 5, 3, 32], stddev=0.01))
+    L1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
     L1 = tf.nn.relu(L1)
-    L1 = tf.nn.max_pool(L1, ksize=[1,2,2,1], strides=[1, 2, 2, 1], padding='SAME')
 
+    '''
+    1st pooling layer
+    input : 96 X 96 X 32
+    output : 48 X 48 X 32
+    '''
+    L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    '''
+    2nd layer
+    input : 48 X 48 X 32
+    output : 48 X 48 X 64
+    '''
     W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
-    L2 = tf.nn.conv2d(L1, W2, strides=[1,1,1,1], padding='SAME')
+    L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
     L2 = tf.nn.relu(L2)
-    L2 = tf.nn.max_pool(L2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-    W3 = tf.Variable(tf.random_normal([7 * 7 * 64, 256], stddev=0.01))
-    L3 = tf.reshape(L2, [-1, 7 * 7 * 64])
+    '''
+    2nd pooling layer
+    input : 48 X 48 X 64
+    output : 16 X 16 X 64
+    '''
+    L2 = tf.nn.max_pool(L2, ksize=[1,3,3,1], strides=[1,3,3,1], padding='SAME')
+
+    # fully connected layer
+    W3 = tf.Variable(tf.random_normal([16 * 16 * 64, 256], stddev=0.01))
+    L3 = tf.reshape(L2, [-1, 16 * 16 * 64])
     L3 = tf.matmul(L3, W3)
     L3 = tf.nn.relu(L3)
     L3 = tf.nn.dropout(L3, keep_prob)
@@ -128,9 +182,27 @@ def build_model():
 def run():
     load_data()
 
-    print(t_train)
+    global X_train
+    global t_train
+    global X_test
+    global t_test
+
+    global X
+    global Y
+
+    print("#1")
+    print("        X_train : ", X_train.shape)
+    print("        t_train : ", t_train.shape)
+    print("        X_test  : ", X_test.shape)
+    print("        t_test  : ", t_test.shape)
 
     model = build_model()
+
+    print("#2")
+    print("        X_train : ", X_train.shape)
+    print("        t_train : ", t_train.shape)
+    print("        X_test  : ", X_test.shape)
+    print("        t_test  : ", t_test.shape)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
     optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 
