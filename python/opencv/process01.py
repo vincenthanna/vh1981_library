9a8be115a54d5e73c49ef66831f26d262084751e
vincenthanna <firsttimelove@gmail.com>
2018-08-20 03:04:39 +0900

    추가수정

--- python/opencv/process01.py
+++ python/opencv/process01.py
@@ -53,11 +53,6 @@ def label_to_onehot(label, depth):
     onehot[label] = 1
     return onehot
 
-print(label_to_onehot(0, 5))
-print(label_to_onehot(1, 5))
-print(label_to_onehot(2, 5))
-
-
 def get_img(imageFilePath):
     image = Image.open(imageFilePath)
     image = image.resize((96,96))
@@ -67,9 +62,7 @@ def get_img(imageFilePath):
 
     return image
 
-
 def load_data():
-    dataIndex = 0
     xtrain = []
     ttrain = []
     xtest = []
@@ -93,13 +86,10 @@ def load_data():
         for f in files:
             imageFilePath = os.path.join(imageDirPath, f)
             image = get_img(imageFilePath)
-            #print("image=", image.shape)
 
-            #label을 one-hot encoding으로 변경해야 함
-            #onehot = tf.one_hot(indices=[label], depth=categoryCnt)
+            #label을 one-hot으로 변경
             onehot = label_to_onehot(label, depth=categoryCnt)
             image = np.reshape(image, [96, 96, 3])
-
 
             if cnt < trainCnt:
                 xtrain.append(image)
@@ -110,8 +100,6 @@ def load_data():
                 ttest.append(onehot)
 
             cnt += 1
-
-            #print("xtrain len=", len(xtrain))
 
             # print("X_train, X_test, t_train ,t_test ", X_train.shape, X_test.shape,
             #       t_train.shape, t_test.shape)
@@ -131,6 +119,66 @@ def load_data():
     print("        t_train : ", t_train.shape)
     print("        X_test  : ", X_test.shape)
     print("        t_test  : ", t_test.shape)
+
+def build_layer(prev, ksize, pdepth, ndepth):
+    W = tf.Variable(tf.random_normal([ksize, ksize, pdepth, ndepth], stddev=0.01))
+    L = tf.nn.conv2d(prev, W, strides=[1, 1, 1, 1], padding='SAME')
+    L = tf.nn.relu(L)
+    return L
+
+def build_model_2():
+    L1 = build_layer(X, 7, 3, 32)
+    L2 = build_layer(L1, 6, 32, 64)
+
+    # 96 X 96 X 64 => 48 X 48 X 64
+    L3 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    L4 = build_layer(L3, 5, 64, 128)
+    L5 = build_layer(L4, 4, 128, 256)
+
+    # 48 X 48 X 256 => 24 X 24 X 256
+    L6 = tf.nn.max_pool(L5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    L7 = build_layer(L6, 3, 256, 512)
+    L8 = build_layer(L7, 2, 512, 1024)
+
+    # 24 => 12
+    L9 = tf.nn.max_pool(L8, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    L10 = build_layer(L9, 1, 1024, 2048)
+
+    # 12 => 6
+    L11 = tf.nn.max_pool(L10, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # fully connected layer
+    W12 = tf.Variable(tf.random_normal([6 * 6 * 2048, 8192], stddev=0.01))
+    L12 = tf.reshape(L11, [-1, 6 * 6 * 2048])
+    L12 = tf.matmul(L12, W12)
+    L12 = tf.nn.relu(L12)
+    L12 = tf.nn.dropout(L12, keep_prob)
+
+    W13 = tf.Variable(tf.random_normal([8192, 2048], stddev=0.01))
+    L13 = tf.reshape(L12, [-1, 8192])
+    L13 = tf.matmul(L13, W13)
+    L13 = tf.nn.relu(L13)
+    L13 = tf.nn.dropout(L13, keep_prob)
+
+    W14 = tf.Variable(tf.random_normal([2048, 512], stddev=0.01))
+    L14 = tf.reshape(L13, [-1, 2048])
+    L14 = tf.matmul(L14, W14)
+    L14 = tf.nn.relu(L14)
+    L14 = tf.nn.dropout(L14, keep_prob)
+
+    W15 = tf.Variable(tf.random_normal([512, 256], stddev=0.01))
+    L15 = tf.reshape(L14, [-1, 512])
+    L15 = tf.matmul(L15, W15)
+    L15 = tf.nn.relu(L15)
+    L15 = tf.nn.dropout(L15, keep_prob)
+
+    W16 = tf.Variable(tf.random_normal([256, categoryCnt], stddev=0.01))
+    model = tf.matmul(L15, W16)
+
+    return model
+
 
 
 def build_model():
@@ -190,19 +238,8 @@ def run():
     global X
     global Y
 
-    print("#1")
-    print("        X_train : ", X_train.shape)
-    print("        t_train : ", t_train.shape)
-    print("        X_test  : ", X_test.shape)
-    print("        t_test  : ", t_test.shape)
+    model = build_model_2()
 
-    model = build_model()
-
-    print("#2")
-    print("        X_train : ", X_train.shape)
-    print("        t_train : ", t_train.shape)
-    print("        X_test  : ", X_test.shape)
-    print("        t_test  : ", t_test.shape)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
     optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 
