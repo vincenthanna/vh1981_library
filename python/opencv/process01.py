fea9089ef6fd8d3d2ed406437a2a02c0c3db21bb
김연희 <vh1981@idis.co.kr>
2018-08-21 23:10:07 +0900

    중간반영

--- python/opencv/process01.py
+++ python/opencv/process01.py
@@ -67,6 +67,9 @@ def load_data():
     ttrain = []
     xtest = []
     ttest = []
+
+    fig = plt.figure(figsize=(10, 10))
+
     for index, imgdir in enumerate(imageDirs):
 
         print(index, imgdir)
@@ -90,6 +93,10 @@ def load_data():
             #label을 one-hot으로 변경
             onehot = label_to_onehot(label, depth=categoryCnt)
             image = np.reshape(image, [96, 96, 3])
+
+            # if (cnt < 100):
+            #     fig.add_subplot(10, 10, cnt + 1)
+            #     plt.imshow(image)
 
             if cnt < trainCnt:
                 xtrain.append(image)
@@ -120,6 +127,8 @@ def load_data():
     print("        X_test  : ", X_test.shape)
     print("        t_test  : ", t_test.shape)
 
+    #plt.show()
+
 def build_layer(prev, ksize, pdepth, ndepth):
     W = tf.Variable(tf.random_normal([ksize, ksize, pdepth, ndepth], stddev=0.01))
     L = tf.nn.conv2d(prev, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -127,6 +136,8 @@ def build_layer(prev, ksize, pdepth, ndepth):
     return L
 
 def build_model_2():
+    global keep_prob
+
     L1 = build_layer(X, 7, 3, 32)
     L2 = build_layer(L1, 6, 32, 64)
 
@@ -179,9 +190,17 @@ def build_model_2():
 
     return model
 
-
+def build_fclayer(prevLayer, inputsize, outputsize):
+    global keep_prob
+    W = tf.Variable(tf.random_normal([inputsize, outputsize], stddev=0.01))
+    L = tf.reshape(prevLayer, [-1, inputsize])
+    L = tf.matmul(L, W)
+    L = tf.nn.relu(L)
+    L = tf.nn.dropout(L, keep_prob)
+    return L
 
 def build_model():
+    global keep_prob
 
     '''
     1st layer
@@ -215,22 +234,184 @@ def build_model():
     L2 = tf.nn.max_pool(L2, ksize=[1,3,3,1], strides=[1,3,3,1], padding='SAME')
 
     # fully connected layer
-    W3 = tf.Variable(tf.random_normal([16 * 16 * 64, 256], stddev=0.01))
-    L3 = tf.reshape(L2, [-1, 16 * 16 * 64])
-    L3 = tf.matmul(L3, W3)
-    L3 = tf.nn.relu(L3)
-    L3 = tf.nn.dropout(L3, keep_prob)
+    # W3 = tf.Variable(tf.random_normal([16 * 16 * 64, 256], stddev=0.01))
+    # L3 = tf.reshape(L2, [-1, 16 * 16 * 64])
+    # L3 = tf.matmul(L3, W3)
+    # L3 = tf.nn.relu(L3)
+    # L3 = tf.nn.dropout(L3, keep_prob)
+    L3 = build_fclayer(L2, 16 * 16 * 64, 256)
+    L4 = build_fclayer(L3, 256, 128)
+    L5 = build_fclayer(L4, 128, 64)
+    L6 = build_fclayer(L5, 64, 32)
 
-    W4 = tf.Variable(tf.random_normal([256, 128], stddev=0.01))
-    L4 = tf.reshape(L3, [-1, 256])
-    L4 = tf.matmul(L4, W4)
-    L4 = tf.nn.relu(L4)
-    L4 = tf.nn.dropout(L4, keep_prob)
+    # W4 = tf.Variable(tf.random_normal([256, 128], stddev=0.01))
+    # L4 = tf.reshape(L3, [-1, 256])
+    # L4 = tf.matmul(L4, W4)
+    # L4 = tf.nn.relu(L4)
+    # L4 = tf.nn.dropout(L4, keep_prob)
 
-    W5 = tf.Variable(tf.random_normal([128, categoryCnt], stddev=0.01))
-    model = tf.matmul(L4, W5)
+    lastW = tf.Variable(tf.random_normal([32, categoryCnt], stddev=0.01))
+    model = tf.matmul(L6, lastW)
 
     return model
+
+
+# convolutional network layer 1
+def conv1(input_data):
+    # layer 1 (convolutional layer)
+    # FLAGS.conv1_filter_size = 3
+    # FLAGS.conv1_layer_size = 16
+    # FLAGS.stride1 = 1
+
+    with tf.name_scope('conv_1'):
+        W_conv1 = tf.Variable(tf.truncated_normal([3, 3, 3, 16], stddev=0.1))
+        b1 = tf.Variable(tf.truncated_normal([16], stddev=0.1))
+        h_conv1 = tf.nn.conv2d(input_data, W_conv1, strides=[1, 1, 1, 1], padding='SAME')
+        h_conv1_relu = tf.nn.relu(tf.add(h_conv1, b1))
+        h_conv1_maxpool = tf.nn.max_pool(h_conv1_relu
+                                         , ksize=[1, 2, 2, 1]
+                                         , strides=[1, 2, 2, 1], padding='SAME')
+
+    return h_conv1_maxpool
+
+
+# convolutional network layer 2
+def conv2(input_data):
+    # FLAGS.conv2_filter_size = 3
+    # FLAGS.conv2_layer_size = 32
+    # FLAGS.stride2 = 1
+
+    with tf.name_scope('conv_2'):
+        W_conv2 = tf.Variable(tf.truncated_normal([3, 3, 16, 32],stddev=0.1))
+        b2 = tf.Variable(tf.truncated_normal([32], stddev=0.1))
+        h_conv2 = tf.nn.conv2d(input_data, W_conv2, strides=[1, 1, 1, 1], padding='SAME')
+        h_conv2_relu = tf.nn.relu(tf.add(h_conv2, b2))
+        h_conv2_maxpool = tf.nn.max_pool(h_conv2_relu
+                                         , ksize=[1, 2, 2, 1]
+                                         , strides=[1, 2, 2, 1], padding='SAME')
+
+    return h_conv2_maxpool
+
+
+# convolutional network layer 3
+def conv3(input_data):
+    # FLAGS.conv3_filter_size = 3
+    # FLAGS.conv3_layer_size = 64
+    # FLAGS.stride3 = 1
+
+    with tf.name_scope('conv_3'):
+        W_conv3 = tf.Variable(tf.truncated_normal([3, 3, 32, 64],stddev=0.1))
+        b3 = tf.Variable(tf.truncated_normal([64], stddev=0.1))
+        h_conv3 = tf.nn.conv2d(input_data, W_conv3, strides=[1, 1, 1, 1], padding='SAME')
+        h_conv3_relu = tf.nn.relu(tf.add(h_conv3, b3))
+        h_conv3_maxpool = tf.nn.max_pool(h_conv3_relu
+                                         , ksize=[1, 2, 2, 1]
+                                         , strides=[1, 2, 2, 1], padding='SAME')
+
+    return h_conv3_maxpool
+
+
+# convolutional network layer 3
+def conv4(input_data):
+    # FLAGS.conv4_filter_size = 5
+    # FLAGS.conv4_layer_size = 128
+    # FLAGS.stride4 = 1
+
+    with tf.name_scope('conv_4'):
+        W_conv4 = tf.Variable(tf.truncated_normal(
+            [5, 5, 64, 128],
+            stddev=0.1))
+        b4 = tf.Variable(tf.truncated_normal(
+            [128], stddev=0.1))
+        h_conv4 = tf.nn.conv2d(input_data, W_conv4, strides=[1, 1, 1, 1], padding='SAME')
+        h_conv4_relu = tf.nn.relu(tf.add(h_conv4, b4))
+        h_conv4_maxpool = tf.nn.max_pool(h_conv4_relu
+                                         , ksize=[1, 2, 2, 1]
+                                         , strides=[1, 2, 2, 1], padding='SAME')
+
+    return h_conv4_maxpool
+
+
+# fully connected layer 1
+def fc1(input_data):
+    # input_layer_size = 6 * 6 * 128
+    # FLAGS.fc1_layer_size = 512
+
+    with tf.name_scope('fc_1'):
+        # 앞에서 입력받은 다차원 텐서를 fcc에 넣기 위해서 1차원으로 피는 작업
+        input_data_reshape = tf.reshape(input_data, [-1, 6 * 6 * 128])
+        W_fc1 = tf.Variable(tf.truncated_normal([6 * 6 * 128, 512], stddev=0.1))
+        b_fc1 = tf.Variable(tf.truncated_normal([512], stddev=0.1))
+        h_fc1 = tf.add(tf.matmul(input_data_reshape, W_fc1), b_fc1)  # h_fc1 = input_data*W_fc1 + b_fc1
+        h_fc1_relu = tf.nn.relu(h_fc1)
+
+    return h_fc1_relu
+
+
+# fully connected layer 2
+def fc2(input_data):
+    # FLAGS.fc2_layer_size = 256
+
+    with tf.name_scope('fc_2'):
+        W_fc2 = tf.Variable(tf.truncated_normal([512, 256], stddev=0.1))
+        b_fc2 = tf.Variable(tf.truncated_normal([256], stddev=0.1))
+        h_fc2 = tf.add(tf.matmul(input_data, W_fc2), b_fc2)  # h_fc1 = input_data*W_fc1 + b_fc1
+        h_fc2_relu = tf.nn.relu(h_fc2)
+
+    return h_fc2_relu
+
+
+# final layer
+def final_out(input_data):
+    global categoryCnt
+    with tf.name_scope('final_out'):
+        W_fo = tf.Variable(tf.truncated_normal([256, categoryCnt], stddev=0.1))
+        b_fo = tf.Variable(tf.truncated_normal([categoryCnt], stddev=0.1))
+        h_fo = tf.add(tf.matmul(input_data, W_fo), b_fo)  # h_fc1 = input_data*W_fc1 + b_fc1
+
+    # 최종 레이어에 softmax 함수는 적용하지 않았다.
+
+    return h_fo
+
+
+# build cnn_graph
+def build_model_3(images, keep_prob):
+    # define CNN network graph
+    # output shape will be (*,48,48,16)
+    r_cnn1 = conv1(images)  # convolutional layer 1
+    print("shape after cnn1 ", r_cnn1.get_shape())
+
+    # output shape will be (*,24,24,32)
+    r_cnn2 = conv2(r_cnn1)  # convolutional layer 2
+    print("shape after cnn2 :", r_cnn2.get_shape())
+
+    # output shape will be (*,12,12,64)
+    r_cnn3 = conv3(r_cnn2)  # convolutional layer 3
+    print("shape after cnn3 :", r_cnn3.get_shape())
+
+    # output shape will be (*,6,6,128)
+    r_cnn4 = conv4(r_cnn3)  # convolutional layer 4
+    print("shape after cnn4 :", r_cnn4.get_shape())
+
+    # fully connected layer 1
+    r_fc1 = fc1(r_cnn4)
+    print("shape after fc1 :", r_fc1.get_shape())
+
+    # fully connected layer2
+    r_fc2 = fc2(r_fc1)
+    print("shape after fc2 :", r_fc2.get_shape())
+
+    ## drop out
+    # 참고 http://stackoverflow.com/questions/34597316/why-input-is-scaled-in-tf-nn-dropout-in-tensorflow
+    # 트레이닝시에는 keep_prob < 1.0 , Test 시에는 1.0으로 한다.
+    r_dropout = tf.nn.dropout(r_fc2, keep_prob)
+    print("shape after dropout :", r_dropout.get_shape())
+
+    # final layer
+    r_out = final_out(r_dropout)
+    print("shape after final layer :", r_out.get_shape())
+
+    return r_out
 
 
 def run():
@@ -244,7 +425,10 @@ def run():
     global X
     global Y
 
-    model = build_model()
+    global keep_prob
+
+    #model = build_model()
+    model = build_model_3(X, keep_prob=keep_prob)
 
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
     optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
@@ -277,7 +461,7 @@ def run():
 
     is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-    print("정확도:", session.run(accuracy, feed_dict={X:X_train, Y:t_train, keep_prob: 1}))
+    print("정확도:", session.run(accuracy, feed_dict={X:X_test, Y:t_test, keep_prob: 1}))
 
 
 
