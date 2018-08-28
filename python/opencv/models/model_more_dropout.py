2ebe393d28141e173448049c86f1155ee01aed72
김연희 <vh1981@idis.co.kr>
2018-08-28 14:42:39 +0900

    수정

--- python/opencv/models/model_more_dropout.py
+++ python/opencv/models/model_more_dropout.py
@@ -69,34 +69,50 @@ def conv4(input_data):
     return h_conv4_maxpool
 
 
-# fully connected layer 1
-def fc1(input_data):
-    with tf.name_scope('fc_1'):
-        # 앞에서 입력받은 다차원 텐서를 fcc에 넣기 위해서 1차원으로 피는 작업
-        input_data_reshape = tf.reshape(input_data, [-1, 6 * 6 * 128])
-        W_fc1 = tf.Variable(tf.truncated_normal([6 * 6 * 128, 512], stddev=0.1))
-        b_fc1 = tf.Variable(tf.truncated_normal([512], stddev=0.1))
-        h_fc1 = tf.add(tf.matmul(input_data_reshape, W_fc1), b_fc1)  # h_fc1 = input_data*W_fc1 + b_fc1
-        h_fc1_relu = tf.nn.relu(h_fc1)
-
-    return h_fc1_relu
 
 
-# fully connected layer 2
-def fc2(input_data):
-    with tf.name_scope('fc_2'):
-        W_fc2 = tf.Variable(tf.truncated_normal([512, 256], stddev=0.1))
-        b_fc2 = tf.Variable(tf.truncated_normal([256], stddev=0.1))
-        h_fc2 = tf.add(tf.matmul(input_data, W_fc2), b_fc2)  # h_fc1 = input_data*W_fc1 + b_fc1
-        h_fc2_relu = tf.nn.relu(h_fc2)
+def fclayer(layername, input, inputsize, outputsize, keep_prob):
+    with tf.name_scope(layername):
 
-    return h_fc2_relu
+        w = tf.Variable(tf.truncated_normal([inputsize, outputsize], stddev=0.1))
+        b = tf.Variable(tf.truncated_normal([outputsize], stddev=0.1))
+        h = tf.add(tf.matmul(input, w), b)  # h_fc1 = input_data*W_fc1 + b_fc1
+        h_relu = tf.nn.relu(h)
+
+        # dropout r_fc1
+        r_dropout_fc1 = tf.nn.dropout(h_relu, keep_prob)
+
+    return h_relu
+
+
+# # fully connected layer 1
+# def fc1(input_data):
+#     with tf.name_scope('fc_1'):
+#         # 앞에서 입력받은 다차원 텐서를 fcc에 넣기 위해서 1차원으로 피는 작업
+#         input_data_reshape = tf.reshape(input_data, [-1, 6 * 6 * 128])
+#         W_fc1 = tf.Variable(tf.truncated_normal([6 * 6 * 128, 1024], stddev=0.1))
+#         b_fc1 = tf.Variable(tf.truncated_normal([1024], stddev=0.1))
+#         h_fc1 = tf.add(tf.matmul(input_data_reshape, W_fc1), b_fc1)  # h_fc1 = input_data*W_fc1 + b_fc1
+#         h_fc1_relu = tf.nn.relu(h_fc1)
+#
+#     return h_fc1_relu
+#
+#
+# # fully connected layer 2
+# def fc2(input_data):
+#     with tf.name_scope('fc_2'):
+#         W_fc2 = tf.Variable(tf.truncated_normal([512, 256], stddev=0.1))
+#         b_fc2 = tf.Variable(tf.truncated_normal([256], stddev=0.1))
+#         h_fc2 = tf.add(tf.matmul(input_data, W_fc2), b_fc2)  # h_fc1 = input_data*W_fc1 + b_fc1
+#         h_fc2_relu = tf.nn.relu(h_fc2)
+#
+#     return h_fc2_relu
 
 
 # final layer
-def final_out(input_data, labelCnt):
+def final_out(input_data, input_size, labelCnt):
     with tf.name_scope('final_out'):
-        W_fo = tf.Variable(tf.truncated_normal([256, labelCnt], stddev=0.1))
+        W_fo = tf.Variable(tf.truncated_normal([input_size, labelCnt], stddev=0.1))
         b_fo = tf.Variable(tf.truncated_normal([labelCnt], stddev=0.1))
         h_fo = tf.add(tf.matmul(input_data, W_fo), b_fo)  # h_fc1 = input_data*W_fc1 + b_fc1
 
@@ -112,45 +128,36 @@ def build_model_more_dropout(images, keep_prob, labelCnt):
     r_cnn1 = conv1(images)  # convolutional layer 1
     print("shape after cnn1 ", r_cnn1.get_shape())
 
-    r_dropout_cnn1 = tf.nn.dropout(r_cnn1, keep_prob)
-
     # output shape will be (*,24,24,32)
-    r_cnn2 = conv2(r_dropout_cnn1)  # convolutional layer 2
+    r_cnn2 = conv2(r_cnn1)  # convolutional layer 2
     print("shape after cnn2 :", r_cnn2.get_shape())
 
-    r_dropout_cnn2 = tf.nn.dropout(r_cnn2, keep_prob)
-
     # output shape will be (*,12,12,64)
-    r_cnn3 = conv3(r_dropout_cnn2)  # convolutional layer 3
+    r_cnn3 = conv3(r_cnn2)  # convolutional layer 3
     print("shape after cnn3 :", r_cnn3.get_shape())
 
-    r_dropout_cnn3 = tf.nn.dropout(r_cnn3, keep_prob)
-
     # output shape will be (*,6,6,128)
-    r_cnn4 = conv4(r_dropout_cnn3)  # convolutional layer 4
+    r_cnn4 = conv4(r_cnn3)  # convolutional layer 4
     print("shape after cnn4 :", r_cnn4.get_shape())
 
-    r_dropout_cnn4 = tf.nn.dropout(r_cnn4, keep_prob)
+    # 앞에서 입력받은 다차원 텐서를 fcc에 넣기 위해서 1차원으로 피는 작업
+    input_data_reshape = tf.reshape(r_cnn4, [-1, 6 * 6 * 128]) #convolution layer 끝난 후 크기를 알아야 한다.
 
-    # fully connected layer 1
-    r_fc1 = fc1(r_dropout_cnn4)
+    # fully-connected layers :
+    r_fc1 = fclayer('r_fc1', input_data_reshape, 6 * 6 * 128, 1024, keep_prob)
     print("shape after fc1 :", r_fc1.get_shape())
 
-    # dropout r_fc1
-    r_dropout_fc1 = tf.nn.dropout(r_fc1, keep_prob)
-
-    # fully connected layer2
-    r_fc2 = fc2(r_dropout_fc1)
+    r_fc2 = fclayer('r_fc2', r_fc1, 1024, 512, keep_prob)
     print("shape after fc2 :", r_fc2.get_shape())
 
-    ## drop out
-    # 참고 http://stackoverflow.com/questions/34597316/why-input-is-scaled-in-tf-nn-dropout-in-tensorflow
-    # 트레이닝시에는 keep_prob < 1.0 , Test 시에는 1.0으로 한다.
-    r_dropout = tf.nn.dropout(r_fc2, keep_prob)
-    print("shape after dropout :", r_dropout.get_shape())
+    r_fc3 = fclayer('r_fc3', r_fc2, 512, 256, keep_prob)
+    print("shape after fc3 :", r_fc3.get_shape())
+
+    r_fc4 = fclayer('r_fc4', r_fc3, 256, 128, keep_prob)
+    print("shape after fc4 :", r_fc4.get_shape())
 
     # final layer
-    r_out = final_out(r_dropout, labelCnt=labelCnt)
+    r_out = final_out(r_fc4, 128, labelCnt=labelCnt)
     print("shape after final layer :", r_out.get_shape())
 
     return r_out
