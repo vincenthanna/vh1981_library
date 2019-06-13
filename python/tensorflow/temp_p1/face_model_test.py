91760dd48d9fc8094e8df4b7575f9a06ae08db30
김연희 <vh1981@idis.co.kr>
2019-06-13 18:46:24 +0900

    임시프로젝트 코드 commit

---
+++ python/tensorflow/temp_p1/face_model_test.py
@@ -0,0 +1,15 @@
+import unittest
+import face_model
+
+class basicTest(unittest.TestCase):
+
+    """이름에 test문구가 들어가 있어야 실행을 해준다."""
+    def test_001(self):
+        pass
+
+    def test_002(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
