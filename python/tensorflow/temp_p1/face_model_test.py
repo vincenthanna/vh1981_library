672cc2093af9741827452e3c1f0f211b68095d18
김연희 <vh1981@idis.co.kr>
2019-06-14 18:20:16 +0900

    ...

--- python/tensorflow/temp_p1/face_model_test.py
+++ python/tensorflow/temp_p1/face_model_test.py
@@ -1,11 +1,15 @@
 import unittest
-import face_model
+from face_model import faceRecoModel
+
+from keras import backend as K
+K.set_image_data_format('channels_first')
 
 class basicTest(unittest.TestCase):
 
     """이름에 test문구가 들어가 있어야 실행을 해준다."""
     def test_001(self):
-        pass
+        model = faceRecoModel(input_shape=(3, 96, 96))
+        print("model output_shape=", model.output_shape)
 
     def test_002(self):
         pass
