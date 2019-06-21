3f303ec90e0ec58eeb800edbe92c208fcfd51d3b
김연희 <vh1981@idis.co.kr>
2019-06-21 18:37:14 +0900

    작업중

--- python/tensorflow/temp_p1/face_model_test.py
+++ python/tensorflow/temp_p1/face_model_test.py
@@ -26,8 +26,9 @@ class basicTest(unittest.TestCase):
 
             self.assertTrue(round(loss.eval(), 3), 528.143)
 
-    def test_expand_dims(self):
+    def aa_test_expand_dims(self):
         with tf.Session() as test:
+
             x = tf.random_normal([10, 4]).eval(session=test)
             print("x.shape:", x.shape)
             a = tf.expand_dims(x, axis=1).eval(session=test)
@@ -40,6 +41,43 @@ class basicTest(unittest.TestCase):
 
             print("a-b.shape", (a-b).shape)
             print((a-b))
+
+
+            a = np.array([1,2])
+            b = np.array([3,4])
+            print(a, b)
+            c = tf.expand_dims(a, axis=1).eval(session=test)
+            print(c)
+            d = tf.expand_dims(a, axis=0).eval(session=test)
+            print(d)
+            print("a-b:")
+            print(c-d)
+
+            a = tf.random_normal([10, 10, 4]).eval(session=test)
+            print(a.shape)
+            a = tf.sqrt(tf.reduce_sum(a, axis=-1))
+            print(a.shape)
+
+    def test_222(self):
+        with tf.Session() as test:
+            a = np.array([
+                [True, False],
+                [False, True]
+            ])
+            print(a.shape)
+
+            b = np.random.rand(2,2)
+            print("b.shape:", b.shape)
+            print(b)
+
+            print("bbb:", tf.boolean_mask(b, a).eval(session=test))
+            bb = tf.map_fn(lambda x : tf.reduce_min(tf.boolean_mask(x[0], x[1])), (tf.cast(b, dtype=tf.float32), a), dtype=tf.float32)
+            print(bb.eval(session=test))
+
+            k = np.random.rand(3, 2)
+            print(tf.reduce_max(k, axis=0).eval(session=test).shape)
+            print(tf.reduce_max(k, axis=1).eval(session=test).shape)
+            print(tf.reduce_max(k, axis=-1).eval(session=test).shape)
 
 
 if __name__ == '__main__':
--- python/tensorflow/temp_p1/main.py
+++ python/tensorflow/temp_p1/main.py
@@ -1,28 +1,40 @@
 import face_model
 import tensorflow as tf
 
-def triplet_loss_func(y_pred, alpha=0.2):
-    """triplet loss 구현 함수
 
-    Arguments:
-        y_pred : anchor, positive, negative vector 리스트
-            anchor : 입력 이미지 vector
-            positive : anchor와 동일인 이미지 vector
-            negative : anchor와 다른사람 이미지 vector
-
-    Returns:
-        loss 값.
-    """
-
-    anchor, pos, neg = y_pred[0], y_pred[1], y_pred[2]
-
-    print(anchor.shape)
-
-    pos_dist = tf.reduce_sum(tf.square(anchor - pos), axis=-1)
-    neg_dist = tf.reduce_sum(tf.square(anchor - neg), axis=-1)
-    # pos_dist가 neg_dist보다 alpha 이상 작으면 loss값이 0이 된다.
-    loss = pos_dist - neg_dist + alpha
-    loss = tf.reduce_sum(tf.maximum(loss, 0.0))
+def all_diffs(a, b):
+    # Returns a tensor of all combinations of a - b
+    print("shapes:", a.shape, b.shape)
+    return tf.expand_dims(a, axis=1) - tf.expand_dims(b, axis=0)
 
 
-    return loss
+def euclidean_dist(embed1, embed2):
+    # Measures the euclidean dist between all samples in embed1 and embed2
+
+    diffs = all_diffs(embed1, embed2)  # get a square matrix of all diffs
+    print("diffs.shape=", diffs.shape)
+    return tf.sqrt(tf.reduce_sum(tf.square(diffs), axis=-1) + 1e-12)
+
+
+TL_MARGIN = 0.2  # The minimum distance margin
+
+
+def triplet_loss(dists, labels):
+    # Defines the "batch hard" triplet loss function.
+    print("labels.shape", labels)
+    same_identity_mask = tf.equal(tf.expand_dims(labels, axis=1),
+                                  tf.expand_dims(labels, axis=0))
+    print("same_identity_mask.shape=", same_identity_mask.shape)
+    aaa = tf.eye(tf.shape(labels)[0], dtype=tf.bool)
+    print("tf.eye.shape=", aaa.shape)
+    negative_mask = tf.logical_not(same_identity_mask)
+    positive_mask = tf.logical_xor(same_identity_mask,
+                                   tf.eye(tf.shape(labels)[0], dtype=tf.bool))
+
+    furthest_positive = tf.reduce_max(dists * tf.cast(positive_mask, tf.float32), axis=1)
+    closest_negative = tf.map_fn(lambda x: tf.reduce_min(tf.boolean_mask(x[0], x[1])),
+                                 (dists, negative_mask), tf.float32)
+
+    diff = furthest_positive - closest_negative
+
+    return tf.maximum(diff + TL_MARGIN, 0.0)
